5f741a84a03e9c35f77782f569f7558b604deadc
Luca Schumacher <lschumacher@pop-os.fritz.box>
2026-05-05 11:50:14 +0200

    Initial Commit

---
+++ src/main/kotlin/com/alleslocker/imoqrestapi/IMoqRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.alleslocker.imoqrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IMoqRestApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<IMoqRestApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/alleslocker/imoqrestapi/IMoqRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alleslocker.imoqrestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IMoqRestApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
